0064ca115ad592142912e275f0e64301cc6d6ab8
VerKse <ksenofontovs.cs@gmail.com>
2020-12-01 20:59:13 +0300

    Initial Commit

---
+++ HW212/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW212
+//
+//  Created by Вера Ксенофонтова on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
